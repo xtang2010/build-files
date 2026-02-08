3c03371fa4b2a242f6e49220449575deae18a1a3
xtang@qnx.com <xtang@qnx.com>
2026-02-09 07:51:39 +0800

    Use "CPUVARDIR" as install path, instead of "CPU"

--- ports/iftop/common.mk
+++ ports/iftop/common.mk
@@ -38,8 +38,8 @@ export PKG_CONFIG_LIBDIR = $(PKG_CONFIG_LIBDIR_IN):$(PKG_CONFIG_TARGET_IN)
 #Config toolchain for qnx
 CONFIGURE_CMD = $(QNX_PROJECT_ROOT)/configure
 CONFIGURE_ARGS = --host=$(CPU)-*-$(OS) \
-                 --prefix=$(IFTOP_INSTALL_ROOT)/$(PREFIX)/$(CPU) \
-                 --exec-prefix=$(IFTOP_INSTALL_ROOT)/$(CPU) \
+                 --prefix=$(IFTOP_INSTALL_ROOT)/$(PREFIX)/$(CPUVARDIR) \
+                 --exec-prefix=$(IFTOP_INSTALL_ROOT)/$(CPUVARDIR) \
                  --srcdir=$(QNX_PROJECT_ROOT) 
 CONFIGURE_ENVS = CFLAGS="$(CFLAGS)" \
                  CXXFLAGS="$(CXXFLAGS)" \
